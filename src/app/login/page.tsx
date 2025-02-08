4e3e914d3cb9818756d75d87c9c80d33f0b16073
Laurehc <111622067+Laurehc@users.noreply.github.com>
2025-02-08 18:41:27 -0500

    Index page con imagen

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,9 @@
 const LoginPage = () => {
-    
+    return(
+    <h1>
+        Hola. Login page. Proximamente...
+    </h1>
+)
 }
+
+export default LoginPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
+import "./globals.css"
+
 const IndexPage = () => {
   return (
     <div className="container">
         <header>
             <div className="logo">
                 <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M24 4H42V17.3333V30.6667H24V44H6V30.6667V17.3333H24V4Z" fill="currentColor"></path>
+                    <path fillRule="evenodd" clipRule="evenodd" d="M24 4H42V17.3333V30.6667H24V44H6V30.6667V17.3333H24V4Z" fill="currentColor"></path>
                 </svg>
                 <h2>ANBU</h2>
             </div>
@@ -12,20 +15,26 @@ const IndexPage = () => {
         <main>
             <section className="fondo">
                 <div>
-                    <img src="ImagenIndex.jpg" 
+                  <img src="/ImagenIndex.jpg" alt="Imagen de ANBU" />
                 </div>
+            </section>
             <section className="hero">
                 <h1>Bienvenido a ANBU</h1>
                 <h2>ANBU es un equipo de operaciones encubiertas de élite. Somos las sombras que protegen la luz. Somos el silencio que impone la paz.</h2>
                 <div className="buttons">
+                  <Link href="./login">
                     <button className="login">Inicia Sesión</button>
+                  </Link>
                 </div>
             </section>
             <section className="join">
                 <p><span className="font-bold">Únete a ANBU</span></p>
                 <p>Se parte de la raíz Konoha, protege el futuro de la aldea desde las sombras y ten el valor de sacrificarte por la voluntad de fuego. </p>
                 <button className="register">Regístrate ahora</button>
+            </section>
         </main>
     </div>
   )
 }
+
+export default IndexPage;
