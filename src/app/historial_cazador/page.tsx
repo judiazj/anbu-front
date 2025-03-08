b2868a179b964bad8abf7b2bd62b6b74cd252000
Selav1401 <slavergne@unal.edu.co>
2025-03-07 21:12:45 -0500

    Montón de cambios

--- src/app/historial_cazador/page.tsx
+++ src/app/historial_cazador/page.tsx
@@ -1,7 +1,11 @@
 "use client";
+
+
+import { Header } from "../components/header/Header";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import "./historial_cazador.css";
+import Link from "next/link";
 
 interface Mission {
   id: number;
@@ -14,19 +18,16 @@ interface Mission {
 export default function MissionsPage() {
   const router = useRouter();
 
-  // Estados vacíos al inicio
   const [missions, setMissions] = useState<Mission[]>([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedEstado, setSelectedEstado] = useState("Todo");
 
-  // Efecto para cargar datos desde el backend (ejemplo con fetch)
   useEffect(() => {
-    // Llamada para misiones
     const fetchMissions = async () => {
       try {
         const response = await fetch("/api/missions");
         const data = await response.json();
-        setMissions(data); // Guardar en estado
+        setMissions(data);
       } catch (error) {
         console.error("Error al obtener misiones:", error);
       }
@@ -35,151 +36,94 @@ export default function MissionsPage() {
     fetchMissions();
   }, []);
 
-  // Filtrado de ejemplo
   const filteredMissions = missions.filter((mission) => {
     const matchSearch = mission.mision.toLowerCase().includes(searchTerm.toLowerCase());
     const matchEstado = selectedEstado === "Todo" || mission.estado === selectedEstado;
     return matchSearch && matchEstado;
   });
 
-  // Handlers
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value);
-  };
-
-  const handleEstadoChange = (estado: string) => {
-    setSelectedEstado(estado);
-  };
-
-  const handleDetalle = (missionId: number) => {
-    router.push(`/misiones/${missionId}`);
-  };
-
-  const handleProfile = () => {
-    router.push("/perfil");
-  };
-
   return (
-
-    <><header>
-      <div className="logo">
-        <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M24 4H42V17.3333V30.6667H24V44H6V30.6667V17.3333H24V4Z"
-            fill="currentColor"
-          ></path>
-        </svg>
-        <h2>ANBU</h2>
-      </div>
-    </header>
-
+    <>
+      <Header />
       <div className="container">
-        {/* Sidebar */}
         <aside className="sidebar">
           <h2>Nombre usuario</h2>
           <nav>
-            <button className="section_button">
-              <img src="/icons/search.svg" />
-              <span>Misiones</span>
-            </button>
             <button className="menu_button">
               <img src="/icons/notification.svg" />
               <span>Notificaciones</span>
             </button>
-            <button className="menu_button" onClick={handleProfile}>
-              <img src="/icons/user.svg" />
-              <span>Perfil</span>
-            </button>
-          </nav>
-        </aside>
+          <Link href="/perfil" className="menu_button">
+            <img src="/icons/user.svg" />
+            <span>Perfil</span>
+          </Link>
+        </nav>
+      </aside>
 
-        {/* Panel derecho */}
-        <div className="rightContainer">
-          <div className="filters">
+      <div className="rightContainer">
+        <div className="filters">
+          {["Todo", "En progreso", "Completado", "Retraso", "Fracaso"].map((estado, index) => (
             <button
-              className={`filterButton 1 ${selectedEstado === "Todo" ? "active" : ""}`}
-              onClick={() => handleEstadoChange("Todo")}
+              key={index}
+              className={`filterButton ${index + 1} ${selectedEstado === estado ? "active" : ""}`}
+              onClick={() => setSelectedEstado(estado)}
             >
-              Todo
+              {estado}
             </button>
-            <button
-              className={`filterButton 2 ${selectedEstado === "En progreso" ? "active" : ""}`}
-              onClick={() => handleEstadoChange("En progreso")}
-            >
-              En progreso
-            </button>
-            <button
-              className={`filterButton 3 ${selectedEstado === "Completado" ? "active" : ""}`}
-              onClick={() => handleEstadoChange("Completado")}
-            >
-              Completado
-            </button>
-            <button
-              className={`filterButton 4 ${selectedEstado === "Retraso" ? "active" : ""}`}
-              onClick={() => handleEstadoChange("Retraso")}
-            >
-              Retraso
-            </button>
-            <button
-              className={`filterButton 5 ${selectedEstado === "Fracaso" ? "active" : ""}`}
-              onClick={() => handleEstadoChange("Fracaso")}
-            >
-              Fracaso
-            </button>
-          </div>
+          ))}
+        </div>
 
-          <div className="searchBar">
-            <img src="/icons/search.svg" className="searchIcon" />
-            <input
-              type="text"
-              placeholder="Buscar misiones..."
-              value={searchTerm}
-              onChange={handleSearchChange}
-              className="searchInput"
-            />
-          </div>
+        <div className="searchBar">
+          <img src="/icons/search.svg" className="searchIcon" />
+          <input
+            type="text"
+            placeholder="Buscar misiones..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="searchInput"
+          />
+        </div>
 
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Misión</th>
-                <th>Estado</th>
-                <th>Fecha de Inicio</th>
-                <th>Fecha Límite</th>
-                <th>Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredMissions.map((mission) => (
-                <tr key={mission.id}>
-                  <td>{mission.mision}</td>
-                  <td>{mission.estado}</td>
-                  <td>{mission.fechaInicio}</td>
-                  <td>{mission.fechaLimite}</td>
-                  <td>
-                    <button className="filterButton" onClick={() => handleDetalle(mission.id)}>
-                      Detalles
-                    </button>
-                  </td>
-                </tr>
-              ))}
-              {/* Ejemplo estático */}
-              <tr>
-                <td>Ejemplo de Misión</td>
-                <td>En progreso</td>
-                <td>2023-01-01</td>
-                <td>2023-12-31</td>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Misión</th>
+              <th>Estado</th>
+              <th>Fecha de Inicio</th>
+              <th>Fecha Límite</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredMissions.map((mission) => (
+              <tr key={mission.id}>
+                <td>{mission.mision}</td>
+                <td>{mission.estado}</td>
+                <td>{mission.fechaInicio}</td>
+                <td>{mission.fechaLimite}</td>
                 <td>
-                  <button className="filterButton" onClick={() => handleDetalle(0)}>
+                  <button className="filterButton" onClick={() => router.push(`/misiones/${mission.id}`)}>
                     Detalles
                   </button>
                 </td>
               </tr>
-            </tbody>
-          </table>
-        </div>
-      </div></>
+            ))}
+
+            <tr>
+              <td>Ejemplo de Misión</td>
+              <td>En progreso</td>
+              <td>2023-01-01</td>
+              <td>2023-12-31</td>
+              <td>
+                <button className="filterButton" onClick={() => router.push(`/misiones/0`)}>
+                  Detalles
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div >
+    </>
   );
 }
--- src/app/notificaciones/page.tsx
+++ src/app/notificaciones/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Card, CardContent } from "@/app/components/ui/cards";
+import Link from 'next/link';
+import { Header } from '../components/header/Header';
 import { Button } from "@/app/components/ui/button";
 import { useState } from "react";
 import "./notificaciones.css";
@@ -13,19 +14,7 @@ export default function Notifications() {
     return (
         <>
             {/* Header */}
-            <header>
-                <div className="logo">
-                    <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            fillRule="evenodd"
-                            clipRule="evenodd"
-                            d="M24 4H42V17.3333V30.6667H24V44H6V30.6667V17.3333H24V4Z"
-                            fill="currentColor"
-                        ></path>
-                    </svg>
-                    <h2>ANBU</h2>
-                </div>
-            </header>
+            <Header />
 
             {/* Contenedor principal para centrar el contenido */}
             <div className="page-container">
@@ -37,10 +26,6 @@ export default function Notifications() {
                             <Button className="menu_button">
                                 <img src="/icons/search.svg" />
                                 <span>Misiones</span>
-                            </Button>
-                            <Button className="section_button">
-                                <img src="/icons/notification_active.svg" />
-                                <span>Notificaciones</span>
                             </Button>
                             <Button className="menu_button">
                                 <img src="/icons/user.svg" />
